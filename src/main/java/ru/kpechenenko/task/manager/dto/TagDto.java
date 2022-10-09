68941bcc0ef56ab49f088177956444018d4c9d1c
Kostya Pechenenko <kpechenenko@gmail.com>
2022-10-09 19:11:56 +0700

    refactor: rename the title to name.

--- src/main/java/ru/kpechenenko/task/manager/dto/TagDto.java
+++ src/main/java/ru/kpechenenko/task/manager/dto/TagDto.java
@@ -13,6 +13,6 @@ import java.util.List;
 @AllArgsConstructor
 public final class TagDto {
     private Integer id;
-    private String title;
+    private String name;
     private List<TaskInput> tasks;
 }
