fdb7c27e2a052b0df6fe0fb44f2a903301b7958d
Kostya Pechenenko <kpechenenko@gmail.com>
2022-10-09 17:35:41 +0700

    init commit

---
+++ src/test/java/ru/kpechenenko/task/manager/TaskManagerApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.kpechenenko.task.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
